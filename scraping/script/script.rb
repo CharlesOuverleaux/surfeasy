963c8ffc65bd92c85fd65c12ac3b6395cec9c580
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-31 17:33:43 +0200

    created the scraping structure and added pseudo code to add it for tomorrow

--- scraping/script/script.rb
+++ scraping/script/script.rb
@@ -1,2 +1,23 @@
 # Create a scraping script to get the ideal conditions of a surf spot
 # from surfline and save it in our data scraping
+require 'open-uri'
+require 'nokogiri'
+
+# we need to find a way to get all the surf spot links
+
+surf_spot = 'ribeira-de-ilhas/5842041f4e65fad6a7708bc2'
+base_url = 'https://www.surfline.com/surf-report/'
+url = base_url + surf_spot
+
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+html_selector = '.sl-ideal-conditions__condition__description'
+
+#this outputs all the content from the ideal conditions
+#we need to divide it/organize it to output a JSON per spot with
+#JSON:  surf spot / swell direction / wind / surf height / tides
+
+html_doc.search(html_selector).each do |element|
+  p element.text.strip
+end
