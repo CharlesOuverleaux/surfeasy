f4b27fb3454018cec5e112d98da5a7ae6ae34881
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-01 15:31:52 +0200

    Scraping completed, getting all model columns

--- scraping/script/script.rb
+++ scraping/script/script.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-# we need to find a way to get all the surf spot links
+# GET ALL THE REGIONS
 regions = [
   'portugal/porto/2735941',
   'portugal/leiria/2267094',
@@ -17,6 +17,8 @@ regions = [
 base_url = 'https://www.surfline.com/surf-reports-forecasts-cams/'
 arr = []
 
+puts "Getting all the spots from the regions"
+
 regions.each do |region|
   url = base_url + region
   html_file = URI.open(url).read
@@ -27,8 +29,10 @@ regions.each do |region|
   end
 end
 
-
+puts "got all the spots from region"
 spots = []
+
+puts "starting to scrape each spot"
 
 arr.each do |link|
   url = 'https://www.surfline.com' + link
@@ -36,37 +40,60 @@ arr.each do |link|
   surfline_id = link_parts.last
   spot_name = link_parts[-2]
   spot = {
+    country: 'Portugal',
     surfline_id: surfline_id,
     spot_name: spot_name
   }
 
   html_file = URI.open(url).read
   html_doc = Nokogiri::HTML(html_file)
+  # IDEAL CONDITIONS SEARCH
   html_selector = '.sl-ideal-conditions__condition__description'
   html_doc.search(html_selector).each do |element|
      key = element.css('h5').text
      value = element.css('p').text
      spot[key] = value
   end
+  # DESCRIPTION
+  description = html_doc.css('.sl-travel-guide__overview__description').text
+  spot['description'] = description
+  # ABILITY LEVEL AND RATING SEARCH
+  # select the container & loop thourgh each div
+  html_doc.search('.sl-travel-guide__overview__ratings div').each do |element|
+    # ABILITY LEVEL & VIBE
+    # select the matching title
+    puts element.css('h3').text
+    if element.css('h3').text == 'Ability Level'
+      level = element.css('h2').text
+      spot['Ability Level'] = level
+      puts level
+    elsif element.css('h3').text == 'Local Vibe'
+      vibe = element.css('h2').text
+      spot['Local Vibe'] = vibe
+      puts vibe
+    end
+  end
   spots << spot
 end
 
-File.open('spots.json', 'wb') do |file|
+File.open('scraping/data/spots.json', 'wb') do |file|
   file.write(JSON.generate({
     spots: spots
   }))
 end
 
+#  Vibe
+
+# X description
+# X Ability level
+# X Country name
 # X id
-#  Country name
 # X Spot name
-#  description
 # X Ideal swell
 # X Ideal wind
 # X Ideal surf height
 # X Ideal tide
-#  Ability level
-#  Vibe
+
 
 # # this outputs all the content from the ideal conditions
 # # we need to divide it/organize it to output a JSON per spot with
