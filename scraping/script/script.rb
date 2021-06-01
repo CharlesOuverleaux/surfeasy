042669b779351ccfe5c8226e4bfdb7046dcaa52b
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-01 16:09:00 +0200

    refactor

--- scraping/script/script.rb
+++ scraping/script/script.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-# GET ALL THE REGIONS
+# fixed regions links
 regions = [
   'portugal/porto/2735941',
   'portugal/leiria/2267094',
@@ -14,65 +14,61 @@ regions = [
   'portugal/faro/2268337'
 ]
 
-base_url = 'https://www.surfline.com/surf-reports-forecasts-cams/'
-arr = []
+# URL's
+base_url = 'https://www.surfline.com'
 
-puts "Getting all the spots from the regions"
-
-regions.each do |region|
-  url = base_url + region
+# Methods
+def get_html_doc(url)
   html_file = URI.open(url).read
-  html_doc = Nokogiri::HTML(html_file)
-  html_selector = '.sl-spot-list__ref a'
-  html_doc.search(html_selector).each do |element|
-    arr << element.attribute('href').value
-  end
+  return Nokogiri::HTML(html_file)
 end
 
+puts "Getting all the spots from the regions"
+spot_links = []
+regions.each do |region|
+  url = "#{base_url}/surf-reports-forecasts-cams/#{region}"
+  html_doc = get_html_doc(url)
+  html_doc.search('.sl-spot-list__ref a').each do |element|
+    spot_links << element.attribute('href').value
+  end
+end
 puts "got all the spots from region"
-spots = []
 
 puts "starting to scrape each spot"
-
-arr.each do |link|
-  url = 'https://www.surfline.com' + link
+spots = []
+spot_links.each do |link|
+  # split link and take: id & spot name
   link_parts = link.split('/')
-  surfline_id = link_parts.last
-  spot_name = link_parts[-2]
   spot = {
     country: 'Portugal',
-    surfline_id: surfline_id,
-    spot_name: spot_name
+    surfline_id: link_parts.last,
+    spot_name: link_parts[-2]
   }
 
-  html_file = URI.open(url).read
-  html_doc = Nokogiri::HTML(html_file)
-  # IDEAL CONDITIONS SEARCH
-  html_selector = '.sl-ideal-conditions__condition__description'
-  html_doc.search(html_selector).each do |element|
+  # scrape spot details
+  url = "#{base_url}#{link}"
+  html_doc = get_html_doc(url)
+  # ideal condition search
+  html_doc.search('.sl-ideal-conditions__condition__description').each do |element|
      key = element.css('h5').text
      value = element.css('p').text
      spot[key] = value
   end
-  # DESCRIPTION
+  # description
   description = html_doc.css('.sl-travel-guide__overview__description').text
   spot['description'] = description
-  # ABILITY LEVEL AND RATING SEARCH
+  # ability and rating
   # select the container & loop thourgh each div
   html_doc.search('.sl-travel-guide__overview__ratings div').each do |element|
-    # ABILITY LEVEL & VIBE
-    # select the matching title
-    puts element.css('h3').text
-    if element.css('h3').text == 'Ability Level'
-      level = element.css('h2').text
-      spot['Ability Level'] = level
-      puts level
-    elsif element.css('h3').text == 'Local Vibe'
-      vibe = element.css('h2').text
-      spot['Local Vibe'] = vibe
-      puts vibe
+    # select the title (Ability or vibe)
+    title = element.css('h3').text
+    # check the matching title
+    if ['Ability Level', 'Local Vibe'].include?(title)
+    # add the info to the spot JSON
+      spot[title] = element.css('h2').text
     end
   end
+  puts "Done scraping spot #{spot[:spot_name]}"
   spots << spot
 end
 
@@ -81,22 +77,4 @@ File.open('scraping/data/spots.json', 'wb') do |file|
     spots: spots
   }))
 end
-
-#  Vibe
-
-# X description
-# X Ability level
-# X Country name
-# X id
-# X Spot name
-# X Ideal swell
-# X Ideal wind
-# X Ideal surf height
-# X Ideal tide
-
-
-# # this outputs all the content from the ideal conditions
-# # we need to divide it/organize it to output a JSON per spot with
-# # JSON:  surf spot / swell direction / wind / surf height / tides
-
 
