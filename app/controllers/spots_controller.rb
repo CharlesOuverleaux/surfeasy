bb370feae2711cd452ff3daf70681964a3b3fe26
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-02 12:42:38 +0200

    fetch current conditions for spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 class SpotsController < ApplicationController
   def index
     @filters = parse_filter_params
@@ -26,11 +28,26 @@ class SpotsController < ApplicationController
   def add_current_spot_data
     @spots.map! do |spot|
       # get conditions for cards
+      conditions = get_current_spot_data(spot)
+
       { data: spot,
-        kpi: calculate_kpi(spot) }
+        kpi: calculate_kpi(spot),
+        wind_speed: conditions[:wind_speed],
+        wave_height: conditions[:wave_height],
+        period: conditions[:period] }
+
       # add to spots
       # and kpi
     end
+  end
+
+  def get_current_spot_data(spot)
+    url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
+    data = JSON.parse(URI.open(url).read)
+    { weather: data['data']['forecasts'][0]['weather'],
+      wind_speed: data['data']['forecasts'][0]['wind']['speed'].to_i,
+      wave_height: data['data']['forecasts'][0]['surf']['max'].round(1),
+      period: data['data']['forecasts'][0]['swells'][0]['period'] }
   end
 
   def sort_by_kpi
