bfc386d1b041cb03cd38465286049ee809796647
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-02 15:49:54 +0200

    refactor controller

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -11,20 +11,16 @@ class SpotsController < ApplicationController
   private
 
   def parse_filter_params
-    { location: [39.598, -9.070],
-      radius: params[:radius] || 30,
-      skill_level: params[:skill_level] }
+    filters = { location: params[:location] || [39.598, -9.070],
+                radius: params[:radius] || 30,
+                skill_level: params[:skill_level] }
+    return filters
   end
 
   def filtered_spots
     # where spot is within radius of location
     # geocoder => finds spots near [lat lon]
     Spot.near(@filters[:location], @filters[:radius], units: :km).to_a
-
-    # calculate fit based on skill level and api data
-    # return filtered spots
-    # for now just take random spots
-    # Spot.order(Arel.sql('RANDOM()')).take(rand(2..3))
   end
 
   def add_current_spot_data
@@ -47,10 +43,10 @@ class SpotsController < ApplicationController
   def current_conditions(spot)
     url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
     data = JSON.parse(URI.open(url).read)
-    result = { weather: data['data']['forecasts'][0]['weather'],
-               wind_speed: data['data']['forecasts'][0]['wind']['speed'].to_i,
-               wave_height: data['data']['forecasts'][0]['surf']['max'].round(1),
-               period: data['data']['forecasts'][0]['swells'][0]['period'] }
+    return { weather: data['data']['forecasts'][0]['weather'],
+             wind_speed: data['data']['forecasts'][0]['wind']['speed'].to_i,
+             wave_height: data['data']['forecasts'][0]['surf']['max'].round(1),
+             period: data['data']['forecasts'][0]['swells'][0]['period'] }
     # Rails.cache.write(url, result)
     # cached = Rails.cache.read(url)
   end
