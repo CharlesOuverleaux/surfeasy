824a1758637e4f9b5619af375e4347c25c2d7cb0
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-02 12:08:20 +0200

    add  controller logic skeleton for filtering spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,44 @@
 class SpotsController < ApplicationController
+  def index
+    @filters = parse_filter_params
+    @spots = filtered_spots
+    add_current_spot_data
+    sort_by_kpi
+  end
+
+  private
+
+  def parse_filter_params
+    { location: { lon: 43.4, lat: 32.1 },
+      radius: params[:radius],
+      skill_level: params[:skill_level] }
+  end
+
+  def filtered_spots
+    # where spot is within radius of location
+    # geocoder?
+    # calculate fit based on skill level and api data
+    # return filtered spots
+    Spot.order(Arel.sql('RANDOM()')).take(10)
+  end
+
+  def add_current_spot_data
+    @spots.map! do |spot|
+      # get data for cards
+      { data: spot,
+        kpi: calculate_kpi(spot) }
+      # add to spots
+      # and kpi
+    end
+  end
+
+  def sort_by_kpi
+    @spots.sort_by! { |spot| - spot[:kpi] }
+  end
+
+  def calculate_kpi(_spot)
+    # calculate kpi for a spot
+    # for now fake it with rand
+    rand(100)
+  end
 end
