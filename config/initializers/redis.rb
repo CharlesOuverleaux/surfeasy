faab06959566c5751c225f7e2f1751f087f6600c
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-03 16:47:33 +0200

    redis config setup

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 $redis = Redis.new
 
 url = ENV["REDISCLOUD_URL"]
-
+puts url
 if url
   Sidekiq.configure_server do |config|
     config.redis = { url: url }
@@ -10,5 +10,6 @@ if url
   Sidekiq.configure_client do |config|
     config.redis = { url: url }
   end
-  @redis ||= Redis.new(:url => (url || 'redis://127.0.0.1:6379'))
+  @redis = Redis.new(url: url) if url
+  @redis = Redis.new(url: 'redis://127.0.0.1:6379') unless url
 end
