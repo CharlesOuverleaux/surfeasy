172a4161b4b6a2919c974eab9bbaa46e1c0d6f97
Maxim Wolotschij <m.wolotschij@web.de>
2021-06-03 11:31:52 +0200

    added a show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   require "sidekiq/web"
   devise_for :users
   root to: 'pages#home'
-  resources :spots, only: [:index]
+  resources :spots, only: [:index, :show] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
