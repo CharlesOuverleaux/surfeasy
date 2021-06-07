10d4af4fbfa8dbf9ce2dab31e819fc9b9d5312f9
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-07 15:05:39 +0200

    fetch spot conditions convert feet to meters

--- app/jobs/fetch_spot_conditions_job.rb
+++ app/jobs/fetch_spot_conditions_job.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'open-uri'
+
 class FetchSpotConditionsJob < ApplicationJob
   queue_as :default
 
@@ -10,12 +11,24 @@ class FetchSpotConditionsJob < ApplicationJob
       url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
       # Get current conditions for each spot
       data = JSON.parse(URI.open(url).read)
-      condition = { weather: data['data']['forecasts'][0]['weather'],
-                    wind_speed: data['data']['forecasts'][0]['wind']['speed'].to_i,
-                    wave_height: data['data']['forecasts'][0]['surf']['max'].round(1),
-                    period: data['data']['forecasts'][0]['swells'][0]['period'] }
+      forecasts = data['data']['forecasts']
+      units = data['units']
+      condition = { weather: forecasts[0]['weather'],
+                    wind_speed: forecasts[0]['wind']['speed'].to_i,
+                    wind_direction: forecasts[0]['wind']['direction'].to_i,
+                    wave_height: standardize_units(forecasts[0]['surf']['max'], units).round(1),
+                    swell_direction: forecasts[0]['swells'][0]['direction'],
+                    period: forecasts[0]['swells'][0]['period'] }
       # cache spot in redis
       $redis.set(spot.surfline_id, JSON.generate(condition))
     end
+  end
+
+  private
+
+  def standardize_units(data, units)
+    return data / 3.2808 unless units['swellHeight'] == 'M'
+
+    return data
   end
 end
