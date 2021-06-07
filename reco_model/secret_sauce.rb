59487b35b8b6dcd4cf0ccddb947100a57a9a875f
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-07 12:38:41 +0200

    Added score model

--- reco_model/secret_sauce.rb
+++ reco_model/secret_sauce.rb
@@ -8,26 +8,65 @@ puts "Retrieved #{spots.count} spots"
 # get data from the api / at the moment test it with mock data
 current_condition = {
   wave_height: 0.5,
-  wave_period: 10,
-  wind_speed: 20,
-  wind_direction: 160,
-  swell_direction: 130
+  wave_period: 17,
+  wind_speed: 5,
+  wind_direction: 90,
+  swell_direction: 325
 }
 
-level = 'beginner'
-# compare ideal data vs live data
-spots.each do |spot|
-  score = 0
-  wave_height = current_condition[:wave_height]
-  if level == 'beginner' && wave_height < 1
-    score += 20
-  elsif level == 'intermediate' && wave_height.between?(0.5, 2)
-    score += 20
-  elsif level == 'advanced' && wave_height > 0.5
-    score += 20
+def wave_height_score(wave_height, level)
+  if level == 'beginner' && wave_height < 1 ||
+     level == 'intermediate' && wave_height.between?(0.5, 2) ||
+     level == 'advanced' && wave_height > 0.5
+    return 20
   end
-  puts "#{spot['spot_name']} wave height: #{wave_height} -- score: #{score}"
+
+  return 0
 end
+
+def wave_period_score(wave_period)
+  return 20 if wave_period > 16
+  return wave_period if wave_period.between?(7, 15)
+
+  return 0
+end
+
+def wind_speed_score(wind_speed)
+  return 0 if wind_speed > 30
+  return 5 if wind_speed > 16
+
+  return 10
+end
+
+def direction_score(dir, ideal_dir, max_diff, score)
+  diff = (dir - ideal_dir) % 360
+  return score if diff < max_diff
+
+  return 0
+end
+
+def calculate_score(spot, level, current_condition)
+  score = 0
+  score += wave_height_score(current_condition[:wave_height], level)
+  score += wave_period_score(current_condition[:wave_period])
+  score += wind_speed_score(current_condition[:wind_speed])
+  score += direction_score(current_condition[:wind_direction], spot['Wind'], 30, 5)
+  score += direction_score(current_condition[:swell_direction], spot['Swell Direction'], 30, 5)
+  puts "#{spot['spot_name']} -- score: #{score}"
+
+  return score
+end
+
+def translate_score(score)
+  return "Amazing" if score.between?(50, 60)
+  return "Very good" if score.between?(40, 49)
+  return "Decent" if score.between?(30, 39)
+  return "Not good" if score < 30
+end
+# compare ideal data vs live data
 # calculate the score
 # give the score to each spot
-# sort results based on the score
+spots.each do |spot|
+  score = calculate_score(spot, 'beginner', current_condition)
+  puts translate_score(score)
+end
