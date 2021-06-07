407eda42083cd8a4a8176494fede1b1b936586ae
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-07 15:26:05 +0200

    finished init profile page and flow

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   end
   resources :reviews, only: [ :destroy ]
 
+  # profile routes
   get '/profile', to: 'profile#index'
   get '/signout', to: 'profile#signout'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
