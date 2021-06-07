c69ba180c4cc05b9bb8fce35c8152585c4688505
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-06-07 15:01:38 +0200

    make view pretty

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,7 @@
 class ProfileController < ApplicationController
   def index
+    # redirect to root if not logged in
+    redirect_to root_path unless current_user
     @user = current_user
   end
 end
