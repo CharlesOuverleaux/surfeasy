1c12af80b9764f66009ec10bd02addf431a47118
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-07 15:23:22 +0200

    refactor

--- app/jobs/fetch_spot_conditions_job.rb
+++ app/jobs/fetch_spot_conditions_job.rb
@@ -8,17 +8,7 @@ class FetchSpotConditionsJob < ApplicationJob
     spots = Spot.all
     $redis.set('created_at', Time.now.to_i)
     spots.each do |spot|
-      url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
-      # Get current conditions for each spot
-      data = JSON.parse(URI.open(url).read)
-      forecasts = data['data']['forecasts']
-      units = data['units']
-      condition = { weather: forecasts[0]['weather'],
-                    wind_speed: forecasts[0]['wind']['speed'].to_i,
-                    wind_direction: forecasts[0]['wind']['direction'].to_i,
-                    wave_height: standardize_units(forecasts[0]['surf']['max'], units).round(1),
-                    swell_direction: forecasts[0]['swells'][0]['direction'],
-                    period: forecasts[0]['swells'][0]['period'] }
+      condition = fetch_spot_condition(spot)
       # cache spot in redis
       $redis.set(spot.surfline_id, JSON.generate(condition))
     end
@@ -26,7 +16,22 @@ class FetchSpotConditionsJob < ApplicationJob
 
   private
 
+  def fetch_spot_condition(spot)
+    url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
+    # Get current conditions for each spot
+    data = JSON.parse(URI.open(url).read)
+    forecasts = data['data']['forecasts']
+    units = data['units']
+    return { weather: forecasts[0]['weather'],
+             wind_speed: forecasts[0]['wind']['speed'].to_i,
+             wind_direction: forecasts[0]['wind']['direction'].to_i,
+             wave_height: standardize_units(forecasts[0]['surf']['max'], units).round(1),
+             swell_direction: forecasts[0]['swells'][0]['direction'],
+             period: forecasts[0]['swells'][0]['period'] }
+  end
+
   def standardize_units(data, units)
+    puts units
     return data / 3.2808 unless units['swellHeight'] == 'M'
 
     return data
