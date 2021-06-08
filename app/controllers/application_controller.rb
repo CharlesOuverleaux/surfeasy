7d8ed3ece6b3f641631ec1142081f336c56cd05f
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-08 16:53:44 +0200

    redirect to same page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :check_page
+  before_action :store_user_location!, if: :storable_location?
 
   private
 
@@ -14,5 +15,18 @@ class ApplicationController < ActionController::Base
     # show interaction icons on spots show
     @header_share_show = false
     @header_share_show = true if @con == 'spots' && @action == 'show'
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
