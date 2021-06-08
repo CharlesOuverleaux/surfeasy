256a770b02ac5032e7c9f3d0c3cbe6fb03e79e47
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-08 17:59:22 +0200

    remove console log

--- app/javascript/plugins/algolia.js
+++ app/javascript/plugins/algolia.js
@@ -2,7 +2,6 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.querySelector(".location_con input")
-  console.log(addressInput);
   if (addressInput) {
     places({ container: addressInput });
   }
