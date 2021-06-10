412e3f1bf413edcab83691501d8850c79ee6799f
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-06-10 09:58:07 +0200

    fix live data fetching

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -89,13 +89,29 @@ class SpotsController < ApplicationController
     return fetch_condition(spot)
   end
 
-  def fetch_condition(spot)
+  def fetch_spot_condition(spot)
     url = "https://services.surfline.com/kbyg/spots/forecasts/?spotId=#{spot.surfline_id}&days=1&intervalHours=3"
+    # Get current conditions for each spot
     data = JSON.parse(URI.open(url).read)
-    return { weather: data['data']['forecasts'][0]['weather'],
-             wind_speed: data['data']['forecasts'][0]['wind']['speed'].to_i,
-             wave_height: data['data']['forecasts'][0]['surf']['max'].round(1),
-             period: data['data']['forecasts'][0]['swells'][0]['period'] }
+    forecasts = data['data']['forecasts']
+    units = data['units']
+    return { weather: forecasts[0]['weather'],
+             wind_speed: in_kph(forecasts[0]['wind']['speed'].to_i, units).to_i,
+             wind_direction: forecasts[0]['wind']['direction'].to_i,
+             wave_height: in_meters(forecasts[0]['surf']['max'], units).round(1),
+             swell_direction: forecasts[0]['swells'][0]['direction'].to_i,
+             period: forecasts[0]['swells'][0]['period'] }
+  end
+
+  def in_meters(data, units)
+    return data / 3.2808 unless units['swellHeight'] == 'M'
+
+    return data
+  end
+  def in_kph(data,units)
+    return data * 1.609 unless units['windSpeed'] == 'KPH'
+
+    return data
   end
 
   def sort_by_kpi
