ae30afe868052be1ae79093b1246231054dd73ff
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-06-21 18:06:33 +0200

    Added system test for homepage and yml file for spots test

--- test/system/spots_test.rb
+++ test/system/spots_test.rb
@@ -1,8 +1,9 @@
 require "application_system_test_case"
 
 class SpotsTest < ApplicationSystemTestCase
-  test "visiting the homepage" do
+  test "should return surfeasy when visiting the homepage" do
     visit root_url # "/"
     assert_selector "h1", text: "Surfeasy"
+    # save_and_open_screenshot
   end
 end
