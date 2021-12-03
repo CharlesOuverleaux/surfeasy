5001f0848b2a6a8a3ed2b622acbbee192a925de1
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-03 11:17:42 +0100

    Redis fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,16 @@
-$redis = Redis.new
-url = ENV["REDISCLOUD_URL"]
-if url
-  Sidekiq.configure_server do |config|
-    config.redis = { url: url }
-  end
+# $redis = Redis.new
+# url = ENV["REDISCLOUD_URL"]
+# if url
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: url }
+#   end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: url }
-  end
-  $redis = Redis.new(url: url) if url
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: url }
+#   end
+#   $redis = Redis.new(url: url) if url
 
-end
+# end
+
+
+$redis = Redis.new(url: ENV["REDISCLOUD_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
