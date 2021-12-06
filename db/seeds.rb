7b25a55fe6aab524a6904c0c6c86a78cc8f293fd
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-06 14:27:30 +0100

    Added weather conditions to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -109,6 +109,35 @@ spots.each do |spot|
 end
 
 puts "\n-----------------------------"
+puts 'Starting to create weather conditions for each spot'
+
+spots_file = File.read('scraping/data/spots.json')
+spots = JSON.parse(spots_file)['spots']
+
+conditions = []
+
+spots.each do |spot|
+  spot_conditions = {
+                  surfline_id: spot['surfline_id'],
+                  weather: {"temperature" => rand(10..25), "condition" => "NIGHT_MOSTLY_CLEAR"},
+                  wind_speed: rand(2..50),
+                  wind_direction: rand(0..360),
+                  wave_height: rand(1..20) / 10.to_f,
+                  swell_direction: rand(220..350),
+                  period: rand(5..20)
+                  }
+  conditions.push(spot_conditions)
+end
+
+File.open('scraping/data/spots_conditions.json', 'wb') do |file|
+  file.write(JSON.generate({
+                             conditions: conditions
+                           }))
+end
+puts "Created surf spots weather conditions JSON 🌦"
+
+puts "\n-----------------------------"
 puts "Seeding done"
 puts "Added #{Spot.count} spots to the database"
 puts "Added #{User.count} users to the database"
+puts "Added weather conditions for each surf spot"
